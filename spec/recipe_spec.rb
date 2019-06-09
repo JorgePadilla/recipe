64487e5330503e85422bb6f00bb37d802b65e8ad
root <root@DESKTOP-EQ2OFGT.localdomain>
2019-06-08 19:14:12 -0600

    suitable tests

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -3,16 +3,29 @@ require_relative "../recipe"
 
 
 describe 'Test Recipe Class' do 
- it 'should get initialize name' do
+ it 'name should not be empty' do
     @recipe= Recipe.new("Hot cake",["Egg", "Flour", "Water", "Sugar"])
-    nameValue = @recipe.name
-    expect(nameValue).not_to be_empty
+    @nameValue = @recipe.name
+    expect(@nameValue).not_to be_empty
  end
 
- it 'should get initialize ingredients' do
-    @recipe= Recipe.new("Hot cake",["Egg", "Flour", "Water", "Sugar"])
-    ingredientsValue = @recipe.ingredients
-    expect(ingredientsValue).not_to be_empty
+ it 'ingredients should not be empty' do
+    @recipe= Recipe.new("Miso Soup",["Tofu", "Green Chard", "Green Onion", "White miso paste"])
+    @ingredientsValue = @recipe.ingredients
+    expect(@ingredientsValue).not_to be_empty
  end
+
+
+   it 'Hot cake should have egg, flour, water, sugar as ingridients' do
+      @recipe= Recipe.new("Hot cake",["Egg", "Flour", "Water", "Sugar"])
+       @ingredients= @recipe.ingredients
+      expect(@ingredients).to match_array(["Egg", "Flour", "Water", "Sugar"])
+   end
+
+   it 'Mino Soup should have green tofu, green chard, green onion, white miso paste as ingridients' do
+      @recipe= Recipe.new("Miso Soup",["Tofu", "Green Chard", "Green Onion", "White miso paste"])
+       @ingredients= @recipe.ingredients
+      expect(@ingredients).to match_array(["Tofu", "Green Chard", "Green Onion", "White miso paste"])
+   end
 end
  
